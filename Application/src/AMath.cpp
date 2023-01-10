9c826b6fb16745c6c48f433d068f5ba178bff1a4
AxinChen <m13647412733@163.com>
2023-01-10 17:53:55 +0800

    数学库追加

--- Application/src/AMath.cpp
+++ Application/src/AMath.cpp
@@ -1,5 +1,54 @@
 #include "AMath.h"
 
+const float Mathf::PI = acosf(-1.0f);
+const float Mathf::Deg2Rad = Mathf::PI / 180.0f;
+const float Mathf::Rad2Deg = 180.0f / Mathf::PI;
+
+float Mathf::FPow(float n, int p)
+{
+	if (p < 0)
+		throw "FPow's p should big than 0";
+	float t = 1.0f;
+	while (p != 0) {
+		if (p & 1)
+			t *= n;
+		p >>= 1;
+		n *= n;
+	}
+	return t;
+}
+
+float Mathf::FPow(int n, int p)
+{
+	if (p < 0)
+		throw "FPow's p should big than 0";
+	int t = 1;
+	while (p != 0) {
+		if (p & 1)
+			t *= n;
+		p >>= 1;
+		n *= n;
+	}
+	return t;
+}
+
+float Mathf::Clamp(float num, float min, float max)
+{
+	if (num < min)
+		return min;
+	else if (num > max)
+		return max;
+	else
+		return num;
+}
+
+float Mathf::Lerp(float from, float to, float k, bool limit)
+{
+	if (limit)
+		k = Saturate(k);
+	return from * k + to * (1 - k);
+}
+
 Vec2::Vec2() : m_x(0), m_y(0)
 {
 }
@@ -18,6 +67,15 @@ Vec2::Vec2(const Vec3& other) : m_x(other.GetX()), m_y(other.GetY())
 
 Vec2::Vec2(const Vec4& other) : m_x(other.GetX()), m_y(other.GetY())
 {
+}
+
+Vec2 Vec2::Normalized() const
+{
+	float len = Len();
+	if (len > 1E-05f)
+		return (*this) * (1 / len);
+	else
+		return Vec2::Zero();
 }
 
 Vec3::Vec3() : m_x(0), m_y(0), m_z(0)
@@ -40,6 +98,15 @@ Vec3::Vec3(const Vec4& other) : m_x(other.GetX()), m_y(other.GetY()), m_z(other.
 {
 }
 
+Vec3 Vec3::Normalized() const
+{
+	float len = Len();
+	if (len > 1E-05f)
+		return (*this) * (1 / len);
+	else
+		return Vec3::Zero();
+}
+
 Vec4::Vec4() : m_x(0), m_y(0), m_z(0), m_w(0)
 {
 }
@@ -58,4 +125,244 @@ Vec4::Vec4(const Vec3& other) : m_x(other.GetX()), m_y(other.GetY()), m_z(other.
 
 Vec4::Vec4(const Vec4& other) : m_x(other.m_x), m_y(other.m_y), m_z(other.m_z), m_w(other.m_w)
 {
+}
+
+Matrix2x2::Matrix2x2()
+{
+	memset(m_p, 0, sizeof(m_p));
+}
+
+Matrix2x2::Matrix2x2(const Matrix2x2& other)
+{
+	memcpy(m_p, &other, sizeof(m_p));
+}
+
+Matrix2x2::Matrix2x2(const Matrix3x3& other)
+{
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			m_p[i][j] = other[i][j];
+}
+
+Matrix2x2::Matrix2x2(const Matrix4x4& other)
+{
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			m_p[i][j] = other[i][j];
+}
+
+Matrix2x2 Matrix2x2::operator+(const Matrix2x2 other) const
+{
+	Matrix2x2 res;
+	float* r = (float*)&res;
+	float* a = (float*)(this);
+	float* b = (float*)&other;
+	for (int i = 0; i < 4; i++) {
+		*r = *a + *b;
+		r++;
+		a++;
+		b++;
+	}
+	return res;
+}
+
+Matrix2x2 Matrix2x2::operator-(const Matrix2x2 other) const
+{
+	Matrix2x2 res;
+	float* r = (float*)&res;
+	float* a = (float*)(this);
+	float* b = (float*)&other;
+	for (int i = 0; i < 4; i++) {
+		*r = *a - *b;
+		r++;
+		a++;
+		b++;
+	}
+	return res;
+}
+
+Matrix2x2 Matrix2x2::operator*(const Matrix2x2 other) const
+{
+	Matrix2x2 res;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			for (int k = 0; k < 2; k++)
+				res[i][j] += m_p[i][k] * other[k][j];
+	return res;
+}
+
+Vec2 Matrix2x2::operator*(const Vec2 vec) const
+{
+	Vec2 res;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			res[i] += m_p[i][j] * vec[j];
+	return res;
+}
+
+Matrix3x3::Matrix3x3()
+{
+	memset(m_p, 0, sizeof(m_p));
+}
+
+Matrix3x3::Matrix3x3(const Matrix2x2& other)
+{
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			m_p[i][j] = other[i][j];
+	for (int i = 0; i < 2; i++) {
+		m_p[2][i] = 0;
+		m_p[i][2] = 0;
+	}
+	m_p[2][2] = 1;
+}
+
+Matrix3x3::Matrix3x3(const Matrix3x3& other)
+{
+	memcpy(m_p, &other, sizeof(m_p));
+}
+
+Matrix3x3::Matrix3x3(const Matrix4x4& other)
+{
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			m_p[i][j] = other[i][j];
+}
+
+Matrix3x3 Matrix3x3::operator+(const Matrix3x3 other) const
+{
+	Matrix3x3 res;
+	float* r = (float*)&res;
+	float* a = (float*)(this);
+	float* b = (float*)&other;
+	for (int i = 0; i < 9; i++) {
+		*r = *a + *b;
+		r++;
+		a++;
+		b++;
+	}
+	return res;
+}
+
+Matrix3x3 Matrix3x3::operator-(const Matrix3x3 other) const
+{
+	Matrix3x3 res;
+	float* r = (float*)&res;
+	float* a = (float*)(this);
+	float* b = (float*)&other;
+	for (int i = 0; i < 9; i++) {
+		*r = *a - *b;
+		r++;
+		a++;
+		b++;
+	}
+	return res;
+}
+
+Matrix3x3 Matrix3x3::operator*(const Matrix3x3 other) const
+{
+	Matrix3x3 res;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			for (int k = 0; k < 3; k++)
+				res[i][j] += m_p[i][k] * other[k][j];
+	return res;
+}
+
+Vec3 Matrix3x3::operator*(const Vec3 vec) const
+{
+	Vec3 res;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			res[i] += m_p[i][j] * vec[j];
+	return res;
+}
+
+Matrix4x4::Matrix4x4()
+{
+	memset(m_p, 0, sizeof(m_p));
+}
+
+Matrix4x4::Matrix4x4(const Matrix2x2& other)
+{
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			m_p[i][j] = other[i][j];
+	for (int i = 0; i < 2; i++) {
+		m_p[2][i] = 0;
+		m_p[i][2] = 0;
+	}
+	for (int i = 0; i < 3; i++) {
+		m_p[3][i] = 0;
+		m_p[i][3] = 0;
+	}
+	m_p[2][2] = 1;
+	m_p[3][3] = 1;
+}
+
+Matrix4x4::Matrix4x4(const Matrix3x3& other)
+{
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			m_p[i][j] = other[i][j];
+	for (int i = 0; i < 3; i++) {
+		m_p[3][i] = 0;
+		m_p[i][3] = 0;
+	}
+	m_p[2][2] = 1;
+	m_p[3][3] = 1;
+}
+
+Matrix4x4::Matrix4x4(const Matrix4x4& other)
+{
+	memcpy(m_p, &other, sizeof(m_p));
+}
+
+Matrix4x4 Matrix4x4::operator+(const Matrix4x4 other) const
+{
+	Matrix4x4 res;
+	float* r = (float*)&res;
+	float* a = (float*)(this);
+	float* b = (float*)&other;
+	for (int i = 0; i < 16; i++) {
+		*r = *a + *b;
+		r++;
+		a++;
+		b++;
+	}
+	return res;
+}
+
+Matrix4x4 Matrix4x4::operator-(const Matrix4x4 other) const
+{
+	Matrix4x4 res;
+	float* r = (float*)&res;
+	float* a = (float*)(this);
+	float* b = (float*)&other;
+	for (int i = 0; i < 16; i++) {
+		*r = *a - *b;
+		r++;
+		a++;
+		b++;
+	}
+	return res;
+}
+
+Matrix4x4 Matrix4x4::operator*(const Matrix4x4 other) const
+{
+	Matrix4x4 res;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+			for (int k = 0; k < 4; k++)
+				res[i][j] += m_p[i][k] * other[k][j];
+	return res;
+}
+
+Vec4 Matrix4x4::operator*(const Vec4 vec) const
+{
+	Vec4 res;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+			res[i] += m_p[i][j] * vec[j];
+	return res;
 }
--- Application/src/AMath.h
+++ Application/src/AMath.h
@@ -2,6 +2,61 @@
 #include <cmath>
 #include <memory>
 
+class Mathf
+{
+public:
+	static inline float Abs(float num) {
+		return fabsf(num);
+	}
+	static inline float Loge(float num) {
+		return logf(num);
+	}
+	static inline float Log(float f, float p) {
+		return logf(p) / logf(f);
+	}
+	static inline float Log10(float num) {
+		return log10f(num);
+	}
+	static inline float Log2(float num) {
+		return log2f(num);
+	}
+	static inline float Sqrt(float num) {
+		return sqrtf(num);
+	}
+	static inline float Pow(float n, float p) {
+		return powf(n, p);
+	}
+	static float FPow(float n, int p);
+	static float FPow(int n, int p);
+	static float Clamp(float num, float min, float max);
+	static inline float Saturate(float num) {
+		return Clamp(num, 0.0f, 1.0f);
+	}
+	static float Lerp(float from, float to, float k, bool limit = true);
+
+	static const float PI;
+	static const float Deg2Rad;
+	static const float Rad2Deg;
+	static inline float Sin(float num) {
+		return sinf(num);
+	}
+	static inline float Cos(float num) {
+		return cosf(num);
+	}
+	static inline float Tan(float num) {
+		return tanf(num);
+	}
+	static inline float Asin(float num) {
+		return asinf(num);
+	}
+	static inline float Acos(float num) {
+		return acosf(num);
+	}
+	static inline float Atan(float num) {
+		return atanf(num);
+	}
+};
+
 class Vec2;
 class Vec3;
 class Vec4;
@@ -20,6 +75,12 @@ public:
 	Vec2(const Vec3& other);
 	Vec2(const Vec4& other);
 
+	inline float& operator[](int n) {
+		return *((float*)this + n);
+	}
+	inline const float& operator[](int n) const {
+		return *((float*)this + n);
+	}
 	inline float GetX() const {
 		return m_x;
 	}
@@ -37,17 +98,29 @@ public:
 		SetY(y);
 	}
 
-	inline float GetDistSquare() const {
+	inline float LenSquare() const {
 		return m_x * m_x + m_y * m_y;
 	}
-	inline float GetDist() const {
-		return sqrtf(GetDistSquare());
+	inline float Len() const {
+		return Mathf::Sqrt(LenSquare());
+	}
+	Vec2 Normalized() const;
+	inline float DistSquare(const Vec2& other) const {
+		float x = m_x - other.m_x, y = m_y - other.m_y;
+		return x * x + y * y;
+	}
+	inline float Dist(const Vec2& other) const {
+		return Mathf::Sqrt(DistSquare(other));
 	}
 
 	inline float Dot(const Vec2& other) const {
 		return this->m_x * other.m_x + this->m_y * other.m_y;
 	}
+	Vec2 Lerp(const Vec2& other, float k, bool limit = true) const;
 	
+	inline Vec2 operator-() const {
+		return Vec2(-m_x, -m_y);
+	}
 	inline Vec2 operator+(const Vec2& other) const {
 		return Vec2(this->m_x + other.m_x, this->m_y + other.m_y);
 	}
@@ -66,9 +139,28 @@ public:
 	inline Vec2 operator*(float num) const {
 		return Vec2(this->m_x * num, this->m_y * num);
 	}
+
+	static Vec2 Zero() {
+		return Vec2();
+	}
+	static Vec2 One() {
+		return Vec2(1.0f, 1.0f);
+	}
+	static Vec2 Up() {
+		return Vec2(0.0f, 1.0f);
+	}
+	static Vec2 Down() {
+		return Vec2(0.0f, -1.0f);
+	}
+	static Vec2 Right() {
+		return Vec2(1.0f, 0.0f);
+	}
+	static Vec2 Left() {
+		return Vec2(-1.0f, 0.0f);
+	}
 };
 
-class Vec3
+class Vec3 final
 {
 private:
 	float m_x, m_y, m_z;
@@ -79,6 +171,12 @@ public:
 	Vec3(const Vec3& other);
 	Vec3(const Vec4& other);
 
+	inline float& operator[](int n) {
+		return *((float*)this + n);
+	}
+	inline const float& operator[](int n) const {
+		return *((float*)this + n);
+	}
 	inline float GetX() const {
 		return m_x;
 	}
@@ -103,11 +201,19 @@ public:
 		SetZ(z);
 	}
 
-	inline float GetDistSqure() const {
+	inline float LenSquare() const {
 		return m_x * m_x + m_y * m_y + m_z * m_z;
 	}
-	inline float GetDist() const {
-		return sqrtf(GetDistSqure());
+	inline float Len() const {
+		return Mathf::Sqrt(LenSquare());
+	}
+	Vec3 Normalized() const;
+	inline float DistSquare(const Vec3& other) const {
+		float x = m_x - other.m_x, y = m_y - other.m_y, z = m_z - other.m_z;
+		return x * x + y * y + z * z;
+	}
+	inline float Dist(const Vec3& other) const {
+		return Mathf::Sqrt(DistSquare(other));
 	}
 
 	inline float Dot(const Vec3& other) const {
@@ -121,6 +227,9 @@ public:
 		);
 	}
 
+	inline Vec3 operator-() const {
+		return Vec3(-m_x, -m_y, -m_z);
+	}
 	inline Vec3 operator+(const Vec3& other) const {
 		return Vec3(this->m_x + other.m_x, this->m_y + other.m_y, this->m_z + other.m_z);
 	}
@@ -139,9 +248,34 @@ public:
 	inline Vec3 operator*(float num) const {
 		return Vec3(this->m_x * num, this->m_y * num, this->m_z * num);
 	}
+
+	static Vec3 Zero() {
+		return Vec3();
+	}
+	static Vec3 One() {
+		return Vec3(1.0f, 1.0f, 1.0f);
+	}
+	static Vec3 Up() {
+		return Vec3(0.0f, 1.0f, 0.0f);
+	}
+	static Vec3 Down() {
+		return Vec3(0.0f, -1.0f, 0.0f);
+	}
+	static Vec3 Right() {
+		return Vec3(1.0f, 0.0f, 0.0f);
+	}
+	static Vec3 Left() {
+		return Vec3(-1.0f, 0.0f, 0.0f);
+	}
+	static Vec3 Front() {
+		return Vec3(0.0f, 0.0f, 1.0f);
+	}
+	static Vec3 Back() {
+		return Vec3(0.0f, 0.0f, -1.0f);
+	}
 };
 
-class Vec4
+class Vec4 final
 {
 private:
 	float m_x, m_y, m_z, m_w;
@@ -152,6 +286,12 @@ public:
 	explicit Vec4(const Vec3& other);
 	Vec4(const Vec4& other);
 
+	inline float& operator[](int n) {
+		return *((float*)this + n);
+	}
+	inline const float& operator[](int n) const {
+		return *((float*)this + n);
+	}
 	inline float GetX() const {
 		return m_x;
 	}
@@ -183,6 +323,9 @@ public:
 		SetW(w);
 	}
 
+	inline Vec4 operator-() const {
+		return Vec4(-m_x, -m_y, -m_z, -m_w);
+	}
 	inline Vec4 operator+(const Vec4& other) const {
 		return Vec4(this->m_x + other.m_x, this->m_y + other.m_y, this->m_z + other.m_z, this->m_w + other.m_w);
 	}
@@ -201,12 +344,19 @@ public:
 	inline Vec4 operator*(float num) const {
 		return Vec4(this->m_x * num, this->m_y * num, this->m_z * num, this->m_w * num);
 	}
+
+	static Vec4 Zero() {
+		return Vec4();
+	}
+	static Vec4 One() {
+		return Vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	}
 };
 
-class Matrix2x2
+class Matrix2x2 final
 {
 private:
-	float m_p[4];
+	float m_p[2][2];
 public:
 	Matrix2x2();
 	Matrix2x2(const Matrix2x2& other);
@@ -214,13 +364,11 @@ public:
 	Matrix2x2(const Matrix4x4& other);
 
 	inline float*& operator[](int row) {
-		float* res;
-		res = m_p + (row << 1);
+		float* res = (float*)(m_p + row);
 		return res;
 	}
 	inline const float*& operator[](int row) const {
-		const float* res;
-		res = m_p + (row << 1);
+		const float* res = (float*)(m_p + row);
 		return res;
 	}
 
@@ -228,12 +376,21 @@ public:
 	Matrix2x2 operator-(const Matrix2x2 other) const;
 	Matrix2x2 operator*(const Matrix2x2 other) const;
 	Vec2 operator*(const Vec2 vec) const;
+
+	static Matrix2x2 Zero() {
+		return Matrix2x2();
+	}
+	static Matrix2x2 One() {
+		Matrix2x2 res;
+		res[1][1] = res[0][0] = 1;
+		return res;
+	}
 };
 
-class Matrix3x3
+class Matrix3x3 final
 {
 private:
-	float m_p[9];
+	float m_p[3][3];
 public:
 	Matrix3x3();
 	explicit Matrix3x3(const Matrix2x2& other);
@@ -241,13 +398,11 @@ public:
 	Matrix3x3(const Matrix4x4& other);
 
 	inline float*& operator[](int row) {
-		float* res;
-		res = m_p + (row << 1);
+		float* res = (float*)(m_p + row);
 		return res;
 	}
 	inline const float*& operator[](int row) const {
-		const float* res;
-		res = m_p + (row << 1);
+		const float* res = (float*)(m_p + row);
 		return res;
 	}
 
@@ -255,12 +410,21 @@ public:
 	Matrix3x3 operator-(const Matrix3x3 other) const;
 	Matrix3x3 operator*(const Matrix3x3 other) const;
 	Vec3 operator*(const Vec3 vec) const;
+
+	static Matrix3x3 Zero() {
+		return Matrix3x3();
+	}
+	static Matrix3x3 One() {
+		Matrix3x3 res;
+		res[2][2] = res[1][1] = res[0][0] = 1;
+		return res;
+	}
 };
 
-class Matrix4x4
+class Matrix4x4 final
 {
 private:
-	float m_p[9];
+	float m_p[4][4];
 public:
 	Matrix4x4();
 	explicit Matrix4x4(const Matrix2x2& other);
@@ -268,13 +432,11 @@ public:
 	Matrix4x4(const Matrix4x4& other);
 
 	inline float*& operator[](int row) {
-		float* res;
-		res = m_p + (row << 1);
+		float* res = (float*)(m_p + row);
 		return res;
 	}
 	inline const float*& operator[](int row) const {
-		const float* res;
-		res = m_p + (row << 1);
+		const float* res = (float*)(m_p + row);
 		return res;
 	}
 
@@ -282,4 +444,13 @@ public:
 	Matrix4x4 operator-(const Matrix4x4 other) const;
 	Matrix4x4 operator*(const Matrix4x4 other) const;
 	Vec4 operator*(const Vec4 vec) const;
+
+	static Matrix4x4 Zero() {
+		return Matrix4x4();
+	}
+	static Matrix4x4 One() {
+		Matrix4x4 res;
+		res[3][3] = res[2][2] = res[1][1] = res[0][0] = 1;
+		return res;
+	}
 };
