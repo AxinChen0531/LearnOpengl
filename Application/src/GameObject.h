7b78f82157d8d46a64e186cb154c20b9259ce7b1
AxinChen <m13647412733@163.com>
2023-02-28 21:09:03 +0800

    fixed

--- Application/src/GameObject.h
+++ Application/src/GameObject.h
@@ -16,23 +16,24 @@
 class GameObject : public Entity, public IAwake, public IAwake1<GameObject*>, public IAwake2<GameObject*, const Vec3&>
 {
 private:
-	static unsigned long long cid;
-	static GameObject SceneRoot;
+	static unsigned long long cid;				//用于组织GO的ID，确保此数永远递增，runntime过程中所有GO不会重号
+	static GameObject SceneRoot;				//场景根物体，作为场景树状更新的起点
 
 private:
-	Vec3 m_position;
-	Vec3 m_Scale;
-	Quaternion m_Rotation;
-	Vec3 m_localPosition;
-	Vec3 m_localScale;
-	Quaternion m_LocalRotation;
+	Vec3 m_position;							//世界坐标系中的坐标，主要用于渲染和场景计算，作为更新时的随变量
+	Vec3 m_Scale;								//世界坐标系中的缩放，主要用于渲染和场景计算，作为更新时的随便量
+	Quaternion m_Rotation;						//世界坐标系中的旋转，主要用于渲染和场景计算，作为更新时的随便量
 
-	unsigned long long m_uid;
-	int m_depth;
-	int m_indexAsChild;
-	GameObject* m_Parent;
-	std::vector<GameObject*> m_children;
-	bool updateFlag;
+	Vec3 m_localPosition;						//相对父物体的坐标，主要用于设置，作为更新时的因变量
+	Vec3 m_localScale;							//相对父物体的缩放，主要用于设置，作为更新时的因变量
+	Quaternion m_LocalRotation;					//相对父物体的旋转，主要用于设置，作为更新时的因变量
+
+	unsigned long long m_uid;					//独有ID，根据cid生成，runtime周期中所有GO不会重复
+	int m_depth;								//GO的场景深度，认为SceneRoot深度为0，每层父子关系会增加1的深度，主要用于在父子关系设置时高效率定位等
+	int m_indexAsChild;							//当前物体为父物体对应的第几个子物体，主要用于高效率处理父子关系变化等
+	GameObject* m_Parent;						//父层物体指针
+	std::vector<GameObject*> m_children;		//子层物体指针
+	bool updateFlag;							//更新flag，为true时需要更新，脏标记算法
 
 public:
 	GameObject();
@@ -61,31 +62,26 @@ public:
 		return m_LocalRotation;
 	}
 
-	inline Vec3& RawPosition() {
-		return m_position;
-	}
-	inline Vec3& RawLocalPosition() {
-		return m_localPosition;
-	}
-	inline Vec3& RawScale() {
-		return m_Scale;
-	}
-	inline Vec3& RawLocalScale() {
-		return m_localScale;
-	}
-	inline Quaternion& RawRotation() {
-		return m_Rotation;
-	}
-	inline Quaternion& RawLocalRotation() {
-		return m_LocalRotation;
-	}
-
+	/// <summary>
+	/// 获取UID
+	/// </summary>
+	/// <returns></returns>
 	inline unsigned long long GetID() const {
 		return m_uid;
 	}
+
+	/// <summary>
+	/// 获取作为父物体的第几个子物体序号，从0开始
+	/// </summary>
+	/// <returns></returns>
 	inline int IndexAsChild() const {
 		return m_indexAsChild;
 	}
+
+	/// <summary>
+	/// 获取子物体数量
+	/// </summary>
+	/// <returns></returns>
 	inline int ChildCount() const {
 		return m_children.size();
 	}
@@ -123,6 +119,14 @@ public:
 	bool MakeIndep(GameObject* child);
 
 private:
+	/// <summary>
+	/// 用于父子关系变化后递归更新深度
+	/// </summary>
+	/// <param name="depth"></param>
 	void UpdateDepth(int depth);
+	/// <summary>
+	/// 用于更新父子关系（不加判断）
+	/// </summary>
+	/// <param name="newParent"></param>
 	void SetParentWithoutJudge(GameObject* newParent);
 };
